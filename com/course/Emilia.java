c897d4f989fe5d2a82c33d737406f0ba2c87bb5a
emili4ciccone <emiliaciccone@gmail.com>
2022-11-02 11:38:59 +0100

    Add sayGoodbye in Emilia

--- com/course/Emilia.java
+++ com/course/Emilia.java
@@ -4,4 +4,8 @@ public class Emilia {
     public static void sayHello() {
         System.out.println("Ciao da Emilia!");
     }
+
+    public static void sayGoodbye() {
+        System.out.println("Arrivederci da Emilia");
+    }
 }
--- com/course/Main.java
+++ com/course/Main.java
@@ -3,5 +3,6 @@ package com.course;
 public class Main {
     public static void main(String[] argv) {
         System.out.println("Hello World!");
+        Emilia.sayHello();
     }
 }
