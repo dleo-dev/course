d960ad2c8649d70a9282fe52935468a43163ab5c
Stefano Baviello <stefano.baviello@teoresigroup.com>
2022-11-02 11:38:54 +0100

    add sayGoodbay in Stefano

--- com/course/Stefano.java
+++ com/course/Stefano.java
@@ -4,4 +4,7 @@ public class Stefano {
     public static void sayHello(){
         System.out.println("Ciao da Stefano!");
     }
+    public static void sayGoodbye(){
+        System.out.println("Arrivederci da Stefano!");
+    }
 }
