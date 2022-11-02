13aec9ae7b3f844d704d6a0ec3f24d12a27ea39d
Andrea Demontis <andreademontis.jobs@gmail.com>
2022-11-02 11:38:44 +0100

    Add sayGoodBye in Andrea

--- com/course/AndreaDemontis.java
+++ com/course/AndreaDemontis.java
@@ -1,7 +1,10 @@
 package com.course;
 
 public class AndreaDemontis {
-    public void Saluta(){
+    public static void Saluta(){
         System.out.println("Ciao da Andrea");
+    }
+    public static void Arrivederci(){
+        System.out.println("Arrivederci da Andrea");
     }
 }
